b80e24f919f179c31efac0735cfc05656589c53e
Tomáš Kotrík <tomas.kotrik@m2ms.sk>
2022-11-14 14:30:23 +0100

    added jquery data tables

--- Gym_Management/Controllers/Account/AccountController.cs
+++ Gym_Management/Controllers/Account/AccountController.cs
@@ -155,40 +155,55 @@ namespace Gym_Management.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
+                ApplicationDbContext applicationDbContext = new ApplicationDbContext();
+                DbSet<Customer> customerList = applicationDbContext.Customers;
+                if (customerList.ToList().Count == 0)
                 {
-
-                    ApplicationDbContext applicationDbContext = new ApplicationDbContext();
-                    DbSet<Customer> customerList = applicationDbContext.Customers;
-                    if (customerList.ToList().Count == 0)
+                    model.Customer.Id = 1;
+                    applicationDbContext.Customers.Add(model.Customer);
+                    await applicationDbContext.SaveChangesAsync();
+                    var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                    var result = await UserManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
                     {
-                            model.Customer.Id = 1;
-                            applicationDbContext.Customers.Add(model.Customer);
-                            await applicationDbContext.SaveChangesAsync();
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                    } else
+                    {
+                        AddErrors(result);
+                    }
+                    
 
+                }
+                else
+                {
+                    Customer customer = customerList.ToList().LastOrDefault();
+                    int id = customer.Id;
+                    model.Customer.Id = id + 1;
+                    applicationDbContext.Customers.Add(model.Customer);
+                    await applicationDbContext.SaveChangesAsync();
+                    var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                    var result = await UserManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
                     }
                     else
                     {
-                        Customer customer = customerList.ToList().LastOrDefault();
-                        int id = customer.Id;
-                        model.Customer.Id = id + 1;
-                        applicationDbContext.Customers.Add(model.Customer);
-                        await applicationDbContext.SaveChangesAsync();
+                        AddErrors(result);
                     }
-
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
                 }
-                AddErrors(result);
+
+
+
+                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                // Send an email with this link
+                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                return RedirectToAction("Index", "Home");
+
+
             }
 
             // If we got this far, something failed, redisplay form
